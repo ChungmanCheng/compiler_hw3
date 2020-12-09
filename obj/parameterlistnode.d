27d0e2781205d1758e21d9ed9c2c5c74bc01831d
chungmancheng <erwinchengchungman916@gmail.com>
2020-12-09 15:07:47 +0800

    make cleaned

--- obj/parameterlistnode.d
+++
@@ -1,4 +0,0 @@
-obj/parameterlistnode.o: lib/parameterlistnode.c \
- include/parameterlistnode.h include/ast.h include/loc.h \
- include/optionalvarnode.h include/identlistnode.h include/typenode.h \
- include/standtypenode.h
--- obj/tailnode.d
+++
@@ -1,5 +0,0 @@
-obj/tailnode.o: lib/tailnode.c include/tailnode.h include/ast.h \
- include/loc.h include/expnode.h include/boolexpnode.h \
- include/simpleexpnode.h include/addnode.h include/termnode.h \
- include/factornode.h include/tailnode.h include/explistnode.h \
- include/numnode.h include/mulnode.h include/relopnode.h include/info.h
